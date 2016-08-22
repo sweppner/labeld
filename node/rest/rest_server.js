a05be19fe47b096c39fdf3a9394d2f38ba045493
Sean M Weppner <sweppner@gmail.com>
2016-08-21 20:43:56 -0400

    Updated README.md with Dependencies and node_server.js with proper naming

--- node/rest/rest_server.js
+++ node/rest/rest_server.js
@@ -63,7 +63,7 @@ var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Labeld listening at http://%s:%s", host, port)
 
 })/**
  * Created by seanweppner on 8/5/16.
